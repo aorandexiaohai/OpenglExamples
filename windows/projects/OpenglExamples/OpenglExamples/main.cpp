4e08eb2c2b17933da038e88fe530b36d03197de8
wuzhenhai <2663976081@qq.com>
2022-07-26 23:26:55 +0800

    update

--- windows/projects/OpenglExamples/OpenglExamples/main.cpp
+++ windows/projects/OpenglExamples/OpenglExamples/main.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+﻿#include <iostream>
 #include "SettingWindow.h"
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
+
+GLFWwindow* InitWindows();
 
 
 void framebuff_size_callback(GLFWwindow* window, int width, int height) {
@@ -15,31 +17,12 @@ void process_input_callabck(GLFWwindow* window, int key, int scancode, int actio
 }
 
 int main() {
-	glfwInit();		
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGLTest", NULL, NULL);
-	if (window == NULL) {
-		std::cout << "Failed to create GLFW window " << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-
-	glfwSetKeyCallback(window, process_input_callabck);
-
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
+	auto window = InitWindows();
 
 	glViewport(0, 0, 800, 600);
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 	glfwSetFramebufferSizeCallback(window, framebuff_size_callback);
-
 	{
 		SettingWindow sw(window);
 		while (!glfwWindowShouldClose(window)) {
@@ -57,4 +40,32 @@ int main() {
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
+}
+
+GLFWwindow* InitWindows()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//绘画区域透明
+	//glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGLTest", NULL, NULL);
+
+	//无边框
+	//glfwSetWindowAttrib(window, GLFW_DECORATED, GLFW_FALSE);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window " << std::endl;
+		glfwTerminate();
+		std::exit(-1);
+	}
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, process_input_callabck);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		std::exit(-1);
+	}
+	return window;
 }
