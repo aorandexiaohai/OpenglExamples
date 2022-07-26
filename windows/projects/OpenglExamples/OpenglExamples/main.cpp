6541eb45cdcc85df4a342c61ddd3fdd4c993df91
wuzhenhai <2663976081@qq.com>
2022-07-26 21:47:14 +0800

    update

--- windows/projects/OpenglExamples/OpenglExamples/main.cpp
+++ windows/projects/OpenglExamples/OpenglExamples/main.cpp
@@ -2,6 +2,9 @@
 
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
+#include "imgui.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
 
 void framebuff_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
@@ -38,6 +41,21 @@ int main() {
 	glViewport(0, 0, 800, 600);
 
 	glfwSetFramebufferSizeCallback(window, framebuff_size_callback);
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
 
 	while (!glfwWindowShouldClose(window)) {
 		//render.
