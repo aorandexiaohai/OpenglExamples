123b6d4aa0b8ef36c7079a15e7b61d8c908c3c08
wuzhenhai <wuzhenhai>
2022-07-28 22:08:42 +0800

    update

--- windows/projects/OpenglExamples/OpenglExamples/render_hello_triangle.cpp
+++ windows/projects/OpenglExamples/OpenglExamples/render_hello_triangle.cpp
@@ -7,13 +7,8 @@ void RenderHelloTriangle::render()
 	// Use the program object
 	program->use();
 	// Load the vertex data
-	
-	CHECK_GL_ERROR();
 	glBindVertexArray(m_vao);
-	CHECK_GL_ERROR();
 	glDrawArrays(GL_TRIANGLES, 0, 3);
-	CHECK_GL_ERROR();
-	glFlush();
 }
 void RenderHelloTriangle::init()
 {
@@ -29,7 +24,6 @@ void RenderHelloTriangle::init()
 	glGenBuffers(1, &m_vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vVertices), vVertices, GL_STATIC_DRAW);
-	program->use();
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
