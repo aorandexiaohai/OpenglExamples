823287f9ee7a85c397b0c1b4415b6b2010a44ae6
wuzhenhai <wuzhenhai>
2022-08-02 20:50:08 +0800

    update

---
+++ windows/projects/OpenglExamples/OpenglExamples/src/transform_texture_render.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "render.h"
+#include "program.h"
+#include <memory>
+class TextureRender : public Render
+{
+private:
+	std::shared_ptr<Program> program{};
+	GLuint m_vao{};
+	GLuint m_vbo{};
+	GLuint m_ebo{};
+	GLuint m_tex{};
+public:
+	TextureRender(const std::string& name_utf8) :Render(name_utf8) {};
+	virtual void render()override;
+	virtual void init() override;
+	virtual void release()override;
+};
